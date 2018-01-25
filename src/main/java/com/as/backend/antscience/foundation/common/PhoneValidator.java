fd649028b2a11fbddbc7614310ec8560d65900ad
lirui <634178463@qq.com>
2018-01-25 23:10:35 +0800

    JWT

---
+++ src/main/java/com/as/backend/antscience/foundation/common/PhoneValidator.java
@@ -0,0 +1,24 @@
+package com.as.backend.antscience.foundation.common;
+
+
+import com.as.backend.antscience.foundation.annotation.Phone;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Created by lirui on 2017/5/16.
+ */
+public class PhoneValidator implements ConstraintValidator<Phone, String> {
+
+    @Override
+    public void initialize(Phone phone) {
+    }
+
+    @Override
+    public boolean isValid(String str, ConstraintValidatorContext constraintValidatorContext) {
+        String regex = "^1\\d{10}$";
+        return str == null || str.matches(regex);
+    }
+
+}
--- src/main/java/com/as/backend/antscience/service/UserServiceImpl.java
+++ src/main/java/com/as/backend/antscience/service/UserServiceImpl.java
@@ -2,9 +2,6 @@ package com.as.backend.antscience.service;
 
 import com.as.backend.antscience.dao.UserDao;
 import com.as.backend.antscience.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,40 +13,8 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Override
-    public void createUser(UserDetails user) {
-        userDao.saveAndFlush(user);
-    }
-
-    @Override
-    public void updateUser(UserDetails user) {
-    }
-
-    @Override
-    public void deleteUser(String username) {
-
-    }
-
-    @Override
-    public void changePassword(String oldPassword, String newPassword) {
-
-    }
-
-    @Override
-    public boolean userExists(String username) {
-        return false;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
-
-    @Override
     public User findUserByUsername(String username) {
         return userDao.findUserByUsername(username);
     }
 
-    public Long getUserIdByUserName(String username){
-        return findUserByUsername(username).getId();
-    }
 }
