6d4cbd14b05a90aa229fcdc7655ea0be99f37c99
lirui <634178463@qq.com>
2018-01-27 20:07:08 +0800

    跨域

--- src/main/java/com/as/backend/antscience/controller/AccountController.java
+++ src/main/java/com/as/backend/antscience/controller/AccountController.java
@@ -4,8 +4,6 @@ import com.as.backend.antscience.dto.LoginUser;
 import com.as.backend.antscience.entity.User;
 import com.as.backend.antscience.enums.Authority;
 import com.as.backend.antscience.service.UserService;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +12,7 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8102", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:8102", maxAge = 3600)
 public class AccountController {
 
     @Resource(name = "userService")
